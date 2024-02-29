ea12e97e34798d8aa205972032307d87d8d8f29b
Timur Usmanov <usmanovtima@gmail.com>
2024-02-29 20:50:24 +0300

    Lesson8

--- OOP/Snake/Snake/Program.cs
+++ OOP/Snake/Snake/Program.cs
@@ -9,19 +9,16 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,2,'*');
-            p1.DrawSymbol();
+            Console.SetWindowSize(80, 25);
 
-            Point p2 = new Point(4, 5, '*');
-            p1.DrawSymbol();
-
-            HorizontalLine horizontalLine = new HorizontalLine(1,10,1,'_');
-            horizontalLine.Draw();
-
-            VerticalLine verticalLine = new VerticalLine(1,2,10,'|');
-            verticalLine.Draw();
-
-            Console.ReadLine();
+            HorizontalLine upLine = new HorizontalLine(0,78,0,'+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
         }
     }
 }
