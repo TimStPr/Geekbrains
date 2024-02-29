b31cabe39da0622a718865516e82a7d0cfa7a008
Timur Usmanov <usmanovtima@gmail.com>
2024-02-29 19:33:20 +0300

    Lesson6

--- OOP/Snake/Snake/Program.cs
+++ OOP/Snake/Snake/Program.cs
@@ -12,6 +12,26 @@ namespace Snake
             Point p1 = new Point(1,2,'*');
             p1.DrawSymbol();
 
+            Point p2 = new Point(4, 5, '*');
+            p1.DrawSymbol();
+
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadLine();
         }
     }
