beba284ab8f6b6ac0072627e9813feab92062541
Timur Usmanov <usmanovtima@gmail.com>
2024-03-01 17:58:38 +0300

    lesson14

--- OOP/Snake/Snake/Program.cs
+++ OOP/Snake/Snake/Program.cs
@@ -11,29 +11,36 @@ namespace Snake
         {
             Console.SetWindowSize(80, 25);
 
-            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
-            Draw(v1);
+            Walls walls =new Walls(80,25);
+            walls.Draw();
 
             Point p = new Point(4, 5, '*');
-            Figure fSnake = new Snake(p, 4, Direction.RIGTH);
-            Draw(fSnake);
-            Snake snake =(Snake)fSnake;
+            Snake snake = new Snake(p, 4, Direction.RIGTH);
+            snake.Draw();
 
-            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
+            FoodCreator foodCreator = new FoodCreator(80,25,'$');
+            Point food = foodCreator.CreateFood();
+            food.DrawSymbol();
 
-            List<Figure> figures = new List<Figure>();
-            figures.Add(snake);
-            figures.Add(v1);
-            figures.Add(h1);
-
-            foreach (var f in figures)
+            while (true) 
             {
-                f.Draw();
-            }
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                    break;
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.DrawSymbol();
+                }
+                else
+                    snake.Move();
 
-            static void Draw (Figure figure)
-            { 
-                figure.Draw();
+                Thread.Sleep(100);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
             }
         }
     }
